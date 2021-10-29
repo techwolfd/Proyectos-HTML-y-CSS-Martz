7cfab9eaab41548e84d179c0c9af6ad98be9cbac
Mmartz1 <93292591+Mmartz1@users.noreply.github.com>
2021-10-29 10:44:46 -0700

    Mayra
    Definiendo colores

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,6 +1,8 @@
 	function mi_primer_boton(){
 			var String = "besame";
 			var result = "String.fontcolor("green");
+			
 			document.getElementById("Irish").innerHTML =
-			result;
+			result; 
+
 		}
